374fffb45875cc4eda327554570f165f2422045c
Thanassis Avgerinos <thanassis.avgerinos@gmail.com>
2024-12-18 05:13:22 -0500

    More improvements.

--- kaizen/__init__.py
+++ kaizen/__init__.py
@@ -4,13 +4,9 @@ import sys
 import git
 import github
 
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
-from langchain_community.document_loaders import GitLoader
-
 from neurosymbolic import compute
 from langchain_community.tools import ReadFileTool, WriteFileTool
+from langchain_community.tools import ListDirectoryTool
 
 
 def main():
@@ -35,29 +31,14 @@ def main():
     owner, project = remote.split(":")[1].split("/")[-2:]
     current_branch = repo.active_branch.name
 
-    # Decide what the most meaningful changes are
-    git_doc_repo = GitLoader(
-        repo_path=".",
-        branch=current_branch
-    ).lazy_load()
-
-    prompt = ChatPromptTemplate.from_messages(
-        [("system", "Suggest a small, yet concrete improvement that should be performed in the following repository. Focus on the code, docs, quality, not menial details like .gitignore. Provide the list of file paths of files that need to be changed (use the git repository metadata you are given to figure out file paths, don't guess) and a brief description of the suggested changes. Here is the repo:\\n\\n{context}")]
-    )
-    llm = ChatOpenAI(model="gpt-4o-mini")
-    chain = create_stuff_documents_chain(llm, prompt)
-    improvements = chain.invoke({"context": git_doc_repo})
-
-    print("Improvements:", improvements)
-
     branch_name = "kaizen"
 
     # Create a branch named `branch_name` and erase any existing branch
     branch = repo.create_head(branch_name, force=True)
     branch.checkout()
 
-    tools = [ReadFileTool(verbose=True), WriteFileTool(verbose=True)]
-    result, _ = compute("I need you to perform modifications on the local git repository." + improvements + "\n\nReturn a title (very short) for the Pull Request describing the changes.", tools)
+    tools = [ListDirectoryTool(verbose=True), ReadFileTool(verbose=True), WriteFileTool(verbose=True)]
+    result, _ = compute("Your current directory is a git repository. Pick a file from the repository and apply a small, yet concrete and important improvement. Edit the files directly. Focus on the code, docs, quality, not menial details like .gitignore. \n\nFinally return a title (very short) for the Pull Request describing the changes you made.", tools)
 
     print("Result:", result)
 
